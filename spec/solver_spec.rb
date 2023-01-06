fecf29b0427bc2b26eaa0e631d3c86044c51c1be
adelguit <adeelguitoun@gmail.com>
2023-01-06 11:21:18 +0100

    Implement unit testing for the reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,7 +4,7 @@ require './calculate_factorial'
 solver = Solver.new
 
 describe Solver do
-  context 'Testing solver methods' do
+  context 'Testing factorial method' do
     it 'Given 0 factorial should return 1' do
       expect(solver.factorial(0)).to eq 1
     end
@@ -15,6 +15,12 @@ describe Solver do
 
     it 'given a negative number factorial should rasie an error' do
       expect { solver.factorial(-1) }.to raise_error(Exception)
+    end
+  end
+
+  context 'Testing reverse method' do
+    it 'Given hello to reverse should return olleh' do
+      expect(solver.reverse("hello")).to eq "olleh"
     end
   end
 end
