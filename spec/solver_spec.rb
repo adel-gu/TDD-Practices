7da97f294531603bf2107319d1b5141dcb80ccbe
Elerqsousy <mahmoud.eleresousy@gmail.com>
2023-01-06 11:41:29 +0200

    Create test cases for calling factorial method with positive number

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,9 +3,21 @@ require "./solver"
 solver = Solver.new
 
 describe Solver do
-  context "Testing solver methods" do
-    it "Given 0 factorial should return 1" do
+  context 'Testing solver methods' do
+    it 'Given 0 factorial should return 1' do
       expect(solver.factorial(0)).to eq 1
+    end
+
+    it 'Given a positive number factorial call the claculate_factorial method' do
+      mock = double('calculate_factorial')
+      expect(mock).to receive(:calculate_factorial).with(5)
+
+      factorial = Solver.new(mock)
+      factorial.factorial(5)
+    end
+
+    it 'given a positive number factorial method should return the factorial of the number' do
+      expect(solver.factorial(3)).to eq 6
     end
   end
 end
