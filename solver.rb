37e7f3eaadfd9b8e837b41a43ae3f19a2be0e51e
Elerqsousy <mahmoud.eleresousy@gmail.com>
2023-01-06 12:07:08 +0200

    Build factorial method case for positive number

--- solver.rb
+++ solver.rb
@@ -1,5 +1,12 @@
+require './calculate_factorial'
+
 class Solver
+
   def factorial(input)
-    return 1 if input = 0
+    return 1 if input == 0
+    if input > 1
+      return calculate_factorial(input)
+    end
+
   end
 end
