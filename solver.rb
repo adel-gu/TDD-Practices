b1ee99bb0718284eb12e44ad385b7afea393a1d4
Elerqsousy <mahmoud.eleresousy@gmail.com>
2023-01-06 12:40:56 +0200

    Build fizzbuzz for cases divisible by only 3 or 5 and nundevisible numbers

--- solver.rb
+++ solver.rb
@@ -17,6 +17,15 @@ class Solver
   def fizzbuzz(num)
     if (num % 3 == 0 && num % 5 == 0)
       return "fizzbuzz"
+
+    elsif num % 3 == 0
+      return "fizz"
+
+    elsif num % 5 == 0
+      return "buzz"
+
+    else
+      return num.to_s
     end
   end
 end
